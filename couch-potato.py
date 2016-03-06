203a322ba0ae7e384d4189ffb08b975a780f2def
Victor Rachieru <vick91r@gmail.com>
2016-03-06 22:27:47 +0200

    Use tvdb api instead of parsing wikipedia

--- couch-potato.py
+++ couch-potato.py
@@ -1,27 +1,31 @@
-from bs4 import BeautifulSoup
-import datetime
-import requests
-import re
+from pytvdbapi import api
 
+tvdb  = api.TVDB("B43FF87DE395DF56")
+shows = ["American Dad", "Family Guy"]
 
-def getTitle(episode):
-	return episode.find_next('td').find_next('td').text
+def getShow(showName):
+  return tvdb.search(showName, "en")[0]
 
-def getAirDate(episode):
-	airDate = episode.find_next('td').find_next('td').find_next('td').find_next('td').find_next('td').find_next('span').find_next('span').string
-	if airDate:
-		match = re.search('(\d+)-(\d+)-(\d+)', airDate)
-		if match:
-			return datetime.date(int(match.group(1)), int(match.group(2)), int(match.group(3))).strftime('%d/%m/%Y')
-	return "unknown"
+def getEpisode(show, episode):
+  print getTitle(show, episode)
+  print getDate(episode)
+  print getDescription(episode)
+  print 
 
-def getEpisodes(url):
-	request = requests.get(url)
-	soup = BeautifulSoup(request.text)
-	for node in soup.find_all('tr', attrs = { 'class':'vevent' }):
-		title = getTitle(node)
-		airDate = getAirDate(node)
-		print(airDate + ":" + title)
+def getTitle(show, episode):
+  return "%s S%sE%s" % (show.SeriesName, episode.SeasonNumber, episode.EpisodeNumber)
 
+def getDate(episode):
+  return episode.FirstAired
 
-getEpisodes("https://en.wikipedia.org/wiki/List_of_Family_Guy_episodes")
+def getDescription(episode):
+  return episode.Overview
+
+def main():
+  for showName in shows:
+    show = getShow(showName)
+    for season in show: 
+      for episode in season:
+        getEpisode(show, episode)
+
+main()
